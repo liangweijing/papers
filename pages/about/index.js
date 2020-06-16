42d856d539389b51b50af67d1ce7a2c48509cc2b
liangweijing <623201572@qq.com>
2020-06-16 22:31:09 +0800

    2020-6-16确定通知弹窗

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,97 +1,106 @@
 const app = getApp()
 
 Component({
-  options: {
-    addGlobalClass: true,
-  },
-  data: {
-    starCount: 0,
-    forksCount: 0,
-    visitTotal: 0,
-  },
-  attached () {
-    console.log("success")
-    let that = this;
-    wx.showLoading({
-      title: '数据加载中',
-      mask: true,
-    })
-    let i = 0;
-    numDH();
-    function numDH () {
-      if (i < 20) {
-        setTimeout(function () {
-          that.setData({
-            starCount: i,
-            forksCount: i,
-            visitTotal: i
-          })
-          i++
-          numDH();
-        }, 20)
-      } else {
-        that.setData({
-          starCount: that.coutNum(3000),
-          forksCount: that.coutNum(484),
-          visitTotal: that.coutNum(24000)
+    options: {
+        addGlobalClass: true,
+    },
+    data: {
+        starCount: 0,
+        forksCount: 0,
+        visitTotal: 0,
+        payQr: '',
+        drQr: '',
+        backgourndImg: '',
+
+    },
+    attached() {
+        console.log("success")
+        let that = this;
+        wx.showLoading({
+            title: '数据加载中',
+            mask: true,
         })
-      }
-    }
-    wx.hideLoading()
-  },
-  ready () {
-    wx.showShareMenu({
-      withShareTicket: true
-    })
-  },
-  methods: {
-    coutNum (e) {
-      if (e > 1000 && e < 10000) {
-        e = (e / 1000).toFixed(1) + 'k'
-      }
-      if (e > 10000) {
-        e = (e / 10000).toFixed(1) + 'W'
-      }
-      return e
-    },
-    CopyLink (e) {
-      wx.setClipboardData({
-        data: e.currentTarget.dataset.link,
-        success: res => {
-          wx.showToast({
-            title: '已复制',
-            duration: 1000,
-          })
+        let i = 0;
+        numDH();
+
+        function numDH() {
+            if (i < 20) {
+                setTimeout(function() {
+                    that.setData({
+                        starCount: i,
+                        forksCount: i,
+                        visitTotal: i
+                    })
+                    i++
+                    numDH();
+                }, 20)
+            } else {
+                that.setData({
+                    starCount: that.coutNum(3000),
+                    forksCount: that.coutNum(484),
+                    visitTotal: that.coutNum(24000)
+                })
+            }
         }
-      })
+        wx.hideLoading()
     },
-    showModal (e) {
-      this.setData({
-        modalName: e.currentTarget.dataset.target
-      })
+    ready() {
+        wx.showShareMenu({
+            withShareTicket: true
+        });
+        app.request('/photoInfo').then(res => {
+            console.log(res)
+            this.setData({
+                payQr: res.data.appreciation_qr,
+                drQr: res.data.dr_qr,
+                backgourndImg: res.data.background,
+            })
+        });
     },
-    hideModal (e) {
-      this.setData({
-        modalName: null
-      })
-    },
-    showQrcode () {
-      wx.previewImage({
-        urls: ['https://ftp.bmp.ovh/imgs/2020/05/45b51aa8c6b2c8d6.jpg'],
-        current: 'https://ftp.bmp.ovh/imgs/2020/05/45b51aa8c6b2c8d6.jpg' // 当前显示图片的http链接      
-      })
-    },
-    // showQrcodeDr () {
-    //   wx.previewImage({
-    //     urls: ['https://ftp.bmp.ovh/imgs/2020/05/b0d9769346e3477f.png'],
-    //     current: 'https://ftp.bmp.ovh/imgs/2020/05/b0d9769346e3477f.png' // 当前显示图片的http链接
-    //   })
-    // },
-    showQrcodeDr () {
-      wx.previewImage({
-        urls: ['https://ftp.bmp.ovh/imgs/2020/05/256e6353994aab2b.gif'],
-        current: 'https://ftp.bmp.ovh/imgs/2020/05/256e6353994aab2b.gif' // 当前显示图片的http链接      
-      })
-    },
-  }
+    methods: {
+        coutNum(e) {
+            if (e > 1000 && e < 10000) {
+                e = (e / 1000).toFixed(1) + 'k'
+            }
+            if (e > 10000) {
+                e = (e / 10000).toFixed(1) + 'W'
+            }
+            return e
+        },
+        CopyLink(e) {
+            wx.setClipboardData({
+                data: e.currentTarget.dataset.link,
+                success: res => {
+                    wx.showToast({
+                        title: '已复制',
+                        duration: 1000,
+                    })
+                }
+            })
+        },
+        showModal(e) {
+            this.setData({
+                modalName: e.currentTarget.dataset.target
+            })
+        },
+        hideModal(e) {
+            this.setData({
+                modalName: null
+            })
+        },
+        // 赞赏
+        showQrcode() {
+            wx.previewImage({
+                urls: [this.data.payQr],
+                current: this.data.payQr // 当前显示图片的http链接      
+            })
+        },
+        // 公众号
+        showQrcodeDr() {
+            wx.previewImage({
+                urls: [this.data.drQr],
+                current: this.data.drQr // 当前显示图片的http链接      
+            })
+        },
+    }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,161 +1,148 @@
 //index.js
-// let bindData = require('../../common/bindData');
-import { $wuxNotification } from '../../miniprogram_npm/wux-weapp/index'
+
 //获取应用实例
 const app = getApp()
 
 Page({
-  data: {
-    CustomBar: app.globalData.CustomBar,
-    journalList: [],
-    keywords: '',
-    shortcut: [],
-    history: [],
-    notify: '',
-  },
-  onLoad () {
-    wx.showShareMenu({
-      withShareTicket: true
-    });
-    this.getList();
-  },
-  onShow () {
-    // 才可赋值给data中数据
-    var self = this;
-    wx.getStorage({
-      key: 'searchWords',
-      success (res) {
-        self.setData({
-          history: JSON.parse(res.data),
-        })
-      }
-    });
-  },
-  showNotification () {
-    this.closeNotification = $wuxNotification().show({
-      image: 'https://cdn.skyvow.cn/logo.png',
-      title: '通知',
-      text: this.data.notify,
-      data: {
-        message: '逗你玩的!!!'
-      },
-      duration: 3000,
-      onClick (data) {
-        console.log(data)
-      },
-      onClose (data) {
-        console.log(data)
-      },
-    })
-  },
-  //事件处理函数
-  hideModal (e) {
-    this.setData({
-      modalName: null
-    })
-  },
-  getList () {
-    app.request('/journals/hot?offset=0&limit=300').then(res => {
-      this.setData({
-        journalList: res.data,
-      })
-      app.request('/notify').then(res => {
-        this.setData({
-          notify: res.data,
-        })
-        this.showNotification();
-      });
-    });
-  },
-  goJournalList (e) {
-    let url = "";
-    if (e && e.currentTarget.id) {
-      url = '../journal_list/index';
-    } else {
-      url = '../journal_list/index?keywords=' + this.data.keywords;
-    }
-    this.setData({
-      modalName: null
-    });
-    wx.navigateTo({
-      url: url,
-    });
-  },
-  goJournalDetail (e) {
-    wx.navigateTo({
-      url: '../journal_detail/index?journal_id=' + e.currentTarget.dataset.id
-    });
-  },
-  showHistory (e) {
-    // console.log(222, e)
-    if (this.data.history.length > 0) {
-      this.setData({
-        modalName: e.currentTarget.dataset.target
-      });
-    }
-  },
-  clickHistory (e) {
-    console.log(e.currentTarget.id);
-    this.setData({
-      keywords: e.currentTarget.id,
-    });
-    this.goJournalList();
-  },
-  inputgetName (e) {
-    let name = e.currentTarget.dataset.keywords;
-    let nameMap = {}
-    nameMap[name] = e.detail && e.detail.value
-    this.setData(nameMap);
-    // console.log(nameMap, e);
-    if (e.detail.value.length > 0) {
-      this.setData({
-        modalName: e.currentTarget.dataset.target
-      });
-      app.request('/journal/search/' + nameMap[name] + '?offset=1&limit=6').then(res => {
-        let tip = "";
-        if (res.data.length == 0) {
-          tip = "抱歉没有找到相关内容";
-        } else {
-          tip = "查看更多搜索结果，请点击'搜索'按钮";
-        }
-        this.setData({
-          shortcut: res.data,
-          tip: tip,
-        });
-      });
-    } else {
-      this.setData({
-        modalName: null,
-        shortcut: [],
-      })
-    }
-  },
-  clearSearch () {
-    this.setData({
-      keywords: '',
-      shortcut: [],
-      modalName: null,
-    });
-  },
-  onHide: function () {
-    if (this.data.keywords) {
-      let arr = this.data.history;
-      if (!this.data.history.includes(this.data.keywords)) {
-        arr.unshift(this.data.keywords);
-        if (arr.length > 5) {
-          arr = arr.length > 5 ? arr.filter((o, index) => index < 5) : arr;
-        }
-        let parseKeywords = JSON.stringify(arr);
-        wx.setStorage({
-          key: "searchWords",
-          data: parseKeywords,
-        });
-      }
-      this.setData({
+    data: {
+        CustomBar: app.globalData.CustomBar,
+        journalList: [],
         keywords: '',
         shortcut: [],
-      })
-    }
+        history: [],
+        notify: '',
+        notifyName: 'notify',
+    },
+    onLoad() {
+        wx.showShareMenu({
+            withShareTicket: true
+        });
+        this.getList();
+    },
+    onShow() {
+        // 才可赋值给data中数据
+        var self = this;
+        wx.getStorage({
+            key: 'searchWords',
+            success(res) {
+                self.setData({
+                    history: JSON.parse(res.data),
+                })
+            }
+        });
+    },
+    //事件处理函数
+    hideModal(e) {
+        this.setData({
+            modalName: null
+        })
+    },
+    hideNotify() {
+        this.setData({
+            notifyName: null
+        })
+    },
+    getList() {
+        app.request('/journals/hot?offset=0&limit=300').then(res => {
+            this.setData({
+                journalList: res.data,
+            })
+            app.request('/notify').then(res => {
+                this.setData({
+                    notify: res.data,
+                })
+            });
+        });
+    },
+    goJournalList(e) {
+        let url = "";
+        if (e && e.currentTarget.id) {
+            url = '../journal_list/index';
+        } else {
+            url = '../journal_list/index?keywords=' + this.data.keywords;
+        }
+        this.setData({
+            modalName: null
+        });
+        wx.navigateTo({
+            url: url,
+        });
+    },
+    goJournalDetail(e) {
+        wx.navigateTo({
+            url: '../journal_detail/index?journal_id=' + e.currentTarget.dataset.id
+        });
+    },
+    showHistory(e) {
+        // console.log(222, e)
+        if (this.data.history.length > 0) {
+            this.setData({
+                modalName: e.currentTarget.dataset.target
+            });
+        }
+    },
+    clickHistory(e) {
+        console.log(e.currentTarget.id);
+        this.setData({
+            keywords: e.currentTarget.id,
+        });
+        this.goJournalList();
+    },
+    inputgetName(e) {
+        let name = e.currentTarget.dataset.keywords;
+        let nameMap = {}
+        nameMap[name] = e.detail && e.detail.value
+        this.setData(nameMap);
+        // console.log(nameMap, e);
+        if (e.detail.value.length > 0) {
+            this.setData({
+                modalName: e.currentTarget.dataset.target
+            });
+            app.request('/journal/search/' + nameMap[name] + '?offset=1&limit=6').then(res => {
+                let tip = "";
+                if (res.data.length == 0) {
+                    tip = "抱歉没有找到相关内容";
+                } else {
+                    tip = "查看更多搜索结果，请点击'搜索'按钮";
+                }
+                this.setData({
+                    shortcut: res.data,
+                    tip: tip,
+                });
+            });
+        } else {
+            this.setData({
+                modalName: null,
+                shortcut: [],
+            })
+        }
+    },
+    clearSearch() {
+        this.setData({
+            keywords: '',
+            shortcut: [],
+            modalName: null,
+        });
+    },
+    onHide: function() {
+        if (this.data.keywords) {
+            let arr = this.data.history;
+            if (!this.data.history.includes(this.data.keywords)) {
+                arr.unshift(this.data.keywords);
+                if (arr.length > 5) {
+                    arr = arr.length > 5 ? arr.filter((o, index) => index < 5) : arr;
+                }
+                let parseKeywords = JSON.stringify(arr);
+                wx.setStorage({
+                    key: "searchWords",
+                    data: parseKeywords,
+                });
+            }
+            this.setData({
+                keywords: '',
+                shortcut: [],
+            })
+        }
 
-  },
+    },
 })
